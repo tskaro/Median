7a66d970e7834fbd80fc05e6ebe52998747e173d
tskaro <tskaro@gmail.com>
2021-03-21 01:53:14 +0400

    cleaned the code

--- median.py
+++ median.py
@@ -1,15 +1,17 @@
-list = [1, 2, 3, 4, 5, 6]
+my_list = [1, 2, 3, 4, 5, 6]
 
-def median(list):
-    n = len(list)
-    list.sort()
+
+def median(data):
+    n = len(data)
+    data.sort()
 
     if n % 2 == 0:
-        median1 = list[n // 2]
-        median2 = list[n // 2 - 1]
-        median = (median1 + median2) / 2
+        median1 = data[n // 2]
+        median2 = data[n // 2 - 1]
+        med = (median1 + median2) / 2
     else:
-        median = list[n // 2]
-    return f"Median for your list is: {str(median)}"
+        med = data[n // 2]
+    return f"Median for your list is: {str(med)}"
 
-print(median(list))
+
+print(median(my_list))
